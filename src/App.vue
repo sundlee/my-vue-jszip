c7627fd07468bc921f0ceab352d3b08df95fbae4
sundlee <sundlee@ncsoft.com>
2022-04-06 21:24:17 +0900

    파일 정보 출력 추가

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,18 @@
       @change="handleFileUpload()"
     >
     <div>
+      <h3 v-if="selectFile">zip file info</h3>
+      <ul v-if="selectFile">
+        <li>lastModified : {{ selectFile.lastModified }}</li>
+        <li>lastModifiedDate : {{ selectFile.lastModifiedDate }}</li>
+        <li>name : {{ selectFile.name }}</li>
+        <li>size(byte) : {{ selectFile.size }}</li>
+        <li>type : {{ selectFile.type }}</li>
+        <li>webkitRelativePath : {{ selectFile.webkitRelativePath }}</li>
+      </ul>
+    </div>
+    <div>
+      <h3 v-if="selectFile">file list</h3>
       <ul>
         <li
           v-for="(name, idx) in fileNames"
@@ -27,23 +39,23 @@ export default {
   name: 'App',
   data() {
     return {
-      file: null,
+      selectFile: null,
       fileNames: [],
     };
   },
   methods: {
     handleFileUpload() {
-      this.file = this.$refs.file.files[0];
-      console.log(this.file.name);
+      this.selectFile = this.$refs.file.files[0];
+      console.log(this.selectFile.name);
 
       var reader = new FileReader()
       reader.onload = function (progressEvent) {
         console.log(progressEvent.target.result);
       }
-      reader.readAsText(this.file);
+      reader.readAsText(this.selectFile);
 
       var new_zip = new JSZip();
-      new_zip.loadAsync(this.file)
+      new_zip.loadAsync(this.selectFile)
         .then((zip) => {
           this.fileNames = Object.keys(zip.files);
           console.log(this.fileNames);
